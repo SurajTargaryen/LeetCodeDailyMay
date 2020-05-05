2183250fb1a44186b945006ee373a791e20ce13c
SurajTargaryen <35169625+SurajTargaryen@users.noreply.github.com>
2020-05-05 18:41:38 +0530

    Number Complement

--- Day4.cpp
+++ Day4.cpp
@@ -1,23 +1,23 @@
+/* ~ operator does the job, but we want to restrict it to our N size.
+So first calculate 'N' size
+then calculate the mask value(max value shifted by 32-'N' size), so that other bits are not needed.
+Finally perform '~' on the number and '&' with mask value */
+
 class Solution {
 public:
     int findComplement(int num) {
-      int count =0;
-      int x=num;
+      int count =0,x=num;
 
       while(x>0)
       {
           count++;
           x=x>>1;
       }
-      cout<<count;
 
       unsigned int temp= ~0;
       temp = temp>>(32-count);
 
-      num = ~num;
-      num = num&temp;
-      cout<<num;
-      cout<<temp;   
-        return num;
+      num = ~num&temp;
+      return num;
     }
 };
